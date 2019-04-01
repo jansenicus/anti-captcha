f01edcf6b78337326e094b6878c181630aac361f
jansenicus <jansen.simanullang@gmail.com>
2019-04-01 15:51:34 +0700

    get and read captcha online

--- tesseract/get_captcha_online.sh
+++ tesseract/get_captcha_online.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-for i in `seq 1 20`;
+# captcha_downloader
+N_CAPTCHAS=2
+for i in `seq 1 $N_CAPTCHAS`;
 do
  curl -s -k -X 'GET' https://devicelocator.bri.co.id/get_captcha.php -o file$i.png
  echo $i
--- tesseract/solve-captcha.py
+++ tesseract/solve-captcha.py
@@ -12,14 +12,17 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('captcha_image_file', \
-type=str, nargs='?', const=1, \
+type=str, nargs='?',  \
 help="Specify captcha image file path")
 args=parser.parse_args()
-print(args.captcha_image_file)
+#print(args.captcha_image_file)
 save_path = '/tmp/'+ args.captcha_image_file.split('/')[-1]
+
+# convert
 subprocess.call(['convert', args.captcha_image_file, '-colorspace', 'gray', '-threshold','50%', save_path])
 subprocess.call(['convert', save_path, '-resize','200%', save_path])
 
+# tesseract
 tesseract_config = '--psm 8 --oem 0 -c tessedit_char_whitelist=abcdefghijklkmnopqrstuvwxyz0145789 --user-words wordlist'
 text_detected = pytesseract.image_to_string(Image.open(save_path),
 config=tesseract_config)
