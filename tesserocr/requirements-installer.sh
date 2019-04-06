3000beaf0454e2494fe37b334783d9c0f45617dd
jansenicus <jansen.simanullang@gmail.com>
2019-04-06 20:31:29 +0700

    requirements installer

---
+++ tesserocr/requirements-installer.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt install tesseract-ocr \
+                 libtesseract-dev \
+                 libleptonica-dev
+pip install Pillow \
+            cython \
+            tesserocr
